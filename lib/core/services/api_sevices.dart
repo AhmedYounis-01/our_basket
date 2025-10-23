454fdb246cad6fc134ca93bb868f33cd69369288
Ahmed Fady <ahmedfady426@gmail.com>
2025-10-23 21:22:15 +0300

    -- add api services

---
+++ lib/core/services/api_sevices.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:e_commerce_supabase/core/services/sensitive_data.dart';
+
+class ApiSevices {
+  final Dio _dio = Dio(
+    BaseOptions(
+      baseUrl: "https://mxgcypikbuuipodksmqe.supabase.co/rest/v1/",
+      headers: {"apikey": anonKey},
+    ),
+  );
+
+  Future<Response> getData(String path) async => await _dio.get(path);
+
+  Future<Response> postData(String path, Map<String, dynamic> data) async =>
+      await _dio.post(path, data: data);
+
+  Future<Response> pathData(String path, Map<String, dynamic> data) async =>
+      await _dio.patch(path, data: data);
+
+  Future<Response> deleteData(String path) async => await _dio.delete(path);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_supabase/core/services/sensitive_data.dart';
 import 'package:e_commerce_supabase/my_observer.dart';
 import 'package:e_commerce_supabase/our_market.dart';
 import 'package:flutter/material.dart';
@@ -12,8 +13,7 @@ void main() async {
     //! Data APIs
     url: "https://mxgcypikbuuipodksmqe.supabase.co",
     //! APIs Keys
-    anonKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14Z2N5cGlrYnV1aXBvZGtzbXFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1OTcxMTQsImV4cCI6MjA3NjE3MzExNH0.gkxgWOEsBd3_obfBMuvTVCFEvvmuDC3PtV3tZlTVSHw",
+    anonKey: anonKey,
   );
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const OurMarket());
